4944c36bd37611760cb82fe071046ce276c2d9a4
TABISH KHAN <tabish003@hotmail.com>
2015-09-08 11:14:17 -0700

    user create action working, added userid to session hash if succesfully created

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ group :development, :test do
 
   gem 'pry'
 
+  gem 'hirb'
+
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    render text: params
+    # render text: params
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to ideas_path, notice: "Sucessfully Registered!"
+      session[:user_id] = @user.id #add user_id to session hash
+      # binding.pry
+    else
+    # flash[:notice] = "Couldnt Create "
+    render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
