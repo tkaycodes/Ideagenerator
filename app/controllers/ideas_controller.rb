714ee2828afe4b2183862cf56804efebef7f4ca7
TABISH KHAN <tabish003@hotmail.com>
2015-09-08 10:01:24 -0700

    added pry gem, formatted each idea as a blockquote on idea index page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,25 @@
 class IdeasController < ApplicationController
+
   def index
     @ideas = Idea.all
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    if @idea.save
+      redirect_to ideas_path, notice: "Idea Saved"
+    else
+      flash[:notice] = "Couldnt Save your Idea :("
+      render 'new'
+    end
+  end  
+
+  private
+  def idea_params
+    params.require(:idea).permit(:name, :body)
   end
 end
