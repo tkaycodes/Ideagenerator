9303ed5978785067efd0b6c514f640978fc7b4fa
TABISH KHAN <tabish003@hotmail.com>
2015-09-08 22:25:39 -0700

    implemented like - destroy aka unlike function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,4 +8,14 @@ class LikesController < ApplicationController
       redirect_to :back, notice: "Couldnt Like"
     end
   end
+
+  def destroy
+    # render text: params
+    @like = Like.where(id: params[:id], idea_id: params[:idea_id])[0]
+    if @like.delete
+      redirect_to :back, notice: "Unliked"
+    else
+      redirect_to :back, notice: "Cant unlike"
+    end
+  end
 end
